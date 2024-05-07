fe86a43eaf3b729d53abe9a7aa69b5994946139f
joygeo007 <64395407+joygeo007@users.noreply.github.com>
2024-05-08 00:01:13 +0530

    updated skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,19 +34,19 @@ const Skills = () => {
    '>
     <motion.div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark lg:p-6 md:p-4 xs:text-xs xs:p-2
     " whileHover={{scale:1.05}}>
-      Python
+      Machine Learning
     </motion.div>
     
-    <Skill name='HTML' x='-20vw' y='-2vw'/>
-    <Skill name='CSS' x='-5vw' y='-10vw'/>
-    <Skill name='Javascript' x='20vw' y='6vw'/>
-    <Skill name='ReactJS' x='0vw' y='12vw'/>
-    <Skill name='NextJS' x='-20vw' y='-15vw'/>
-    <Skill name='GatsbyJS' x='15vw' y='-12vw'/>
-    <Skill name='Web Design' x='32vw' y='-5vw'/>
-    <Skill name='Figma' x='0vw' y='-20vw'/>
-    <Skill name='Firebase' x='-30vw' y='9vw'/>
-    <Skill name='Tailwind' x='18vw' y='18vw'/>
+    <Skill name='Python' x='-20vw' y='-2vw'/>
+    <Skill name='Tensorflow' x='-5vw' y='-10vw'/>
+    <Skill name='Scikit-Learn' x='20vw' y='6vw'/>
+    <Skill name='Pytorch' x='0vw' y='12vw'/>
+    <Skill name='Pandas' x='-20vw' y='-15vw'/>
+    <Skill name='Numpy' x='15vw' y='-12vw'/>
+    <Skill name='Deep Learning' x='32vw' y='-5vw'/>
+    <Skill name='NLP' x='0vw' y='-20vw'/>
+    <Skill name='Data Viz' x='-30vw' y='9vw'/>
+    <Skill name='SQL/NoSQL' x='18vw' y='18vw'/>
 
    </div>
    </>
